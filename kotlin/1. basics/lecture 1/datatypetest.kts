2e6baac575b2d4fc71ed2546cc85367a48dddf4e
Vishwesh Aryan <148064350+Int-vishwesh@users.noreply.github.com>
2025-11-22 16:01:31 +0530

    problem in local run plx use compiler

---
+++ kotlin/1. basics/lecture 1/datatypetest.kts
@@ -0,0 +1,14 @@
+fun main() 
+{
+    val n: Int = 42
+    val d: Double = 3.14652
+    val c: Char = 'K'
+    val b: Boolean = true
+    val s: String = "Hello, Kotlin!"
+
+    println("Integer: $n")
+    println("Double: $d")
+    println("Character: $c")
+    println("Boolean: $b")
+    println("String: $s")
+}
