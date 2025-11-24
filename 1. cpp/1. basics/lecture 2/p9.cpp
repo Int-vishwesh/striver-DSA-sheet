d69a70555ea7ad527883184a6bb0022be2fbe763
Vishwesh Aryan <148064350+Int-vishwesh@users.noreply.github.com>
2025-11-24 23:39:47 +0530

    pattern 9

--- 1. cpp/1. basics/lecture 2/p9.cpp
+++ 1. cpp/1. basics/lecture 2/p9.cpp
@@ -4,21 +4,34 @@ using namespace std;
 class solution {
 public:
     solution(int n) {
-      for(int i=n; i>=1; i--) {
-        for(int j=1; j<=i; j++) {
-          cout<<"* ";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                cout << " ";
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                cout << "* ";
+            }
+            cout << endl;
         }
-        cout<<endl;
-      }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                cout << " ";
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
     }
 };
 
 /* pattern output:
 
-* * * *
-* * *
-* *
-*
+    *
+  * * *
+* * * * *
+  * * *
+    *
 
 */
 
--- 2. python/1. basics/lecture 2/p9.py
+++ 2. python/1. basics/lecture 2/p9.py
@@ -1,16 +1,19 @@
 class Solution:
     def __init__(self, n): #constructor
         for i in range(n):
-            for j in range(n):
-                print("* ", end="")
-            print()
+            print(" " * (n - i - 1), end="")
+            print("* " * (2 * i + 1))
+        for i in range(n - 2, -1, -1):
+            print(" " * (n - i - 1), end="")
+            print("* " * (2 * i + 1))
 
 ''' pattern output:
 
-* * * *
-* * * *
-* * * *
-* * * *
+    *
+  * * *
+* * * * *
+  * * *
+    *
 
 '''
 
