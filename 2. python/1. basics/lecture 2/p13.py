5f005a9ee698c4ab6bafe460f2e0f5c061bff225
Vishwesh Aryan <148064350+Int-vishwesh@users.noreply.github.com>
2025-11-30 17:35:33 +0530

    p13

--- 2. python/1. basics/lecture 2/p13.py
+++ 2. python/1. basics/lecture 2/p13.py
@@ -1,28 +1,23 @@
 class Solution:
     def __init__(self, n): #constructor
-        for i in range(n):
-            for j in range(i + 1):
-                print("*", end=" ")
-            print()
-        for i in range(n - 1):
-            for j in range(n - i - 1):
-                print("*", end=" ")
+        count = 1
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                print(count, end=' ')
+                count += 1
             print()
 
 ''' pattern output:
 
-* 
-* *
-* * *
-* * * *
-* * *
-* *
-*
+1
+2 3
+4 5 6
+7 8 9 10
 
 '''
 
 if __name__ == "__main__":
     size = int(input("enter size: "))
 
-    print("1. output:")
+    print("13. output:")
     Solution(size)
